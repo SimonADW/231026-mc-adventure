3e54867fe4271657d6bf96e27cabdee735eb4b87
SimonADW <simon-winter@outlook.com>
2023-11-13 11:38:33 +0100

    Add open close drawer functionality

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
 // Menu Drawer
+const hamburgerButton = document.querySelector(".header__hamburger-button");
+const menuDrawer = document.querySelector(".menu-drawer")
+const closeDrawerButton = document.querySelector(".menu-drawer__close")
 
+hamburgerButton.addEventListener("click", ()=> {
+	menuDrawer.classList.toggle("menu-drawer--visible");
+});
 
-
+closeDrawerButton.addEventListener("click", ()=> {
+	menuDrawer.classList.toggle("menu-drawer--visible");
+});
 
 // Question Drawer
 
