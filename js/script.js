efab38f451c56e18c90c9026b18736c7f1b420f3
SimonADW <simon-winter@outlook.com>
2023-11-15 16:57:08 +0100

    Write pop-drawer logic

--- js/script.js
+++ js/script.js
@@ -11,7 +11,44 @@ closeDrawerButton.addEventListener("click", ()=> {
 	menuDrawer.classList.toggle("menu-drawer--visible");
 });
 
-// Question Drawer
+// Pop Drawer
+const closePopDrawerButton = document.querySelector(".pop-drawer__close")
+const popDrawer = document.querySelector(".pop-drawer")
+const accordionButton = document.querySelector(".pop-drawer__accordion-button");
+const accordionContent = document.querySelector(".pop-drawer__accordion-content");
+const accordionContentButtons = document.querySelectorAll(".pop-drawer__accordion-content__button");
+const thanksMessage = document.querySelector(".pop-drawer__thanks");
+
+window.addEventListener("DOMContentLoaded", ()=> {
+	setTimeout(()=> {
+		popDrawer.classList.add("pop-drawer__visible");
+	}, 300)
+})
+
+closePopDrawerButton.addEventListener("click", ()=>{
+	popDrawer.classList.remove("pop-drawer__visible");
+})
+
+const toggleAccordionContent = ()=> {
+	thanksMessage.style.display = "none";
+	accordionContent.classList.toggle("pop-drawer__accordion-content--visible");
+	accordionButton.classList.toggle("pop-drawer__accordion-button--expanded");
+}
+
+const closeAndDisplayThanks = ()=> {
+	toggleAccordionContent();
+	thanksMessage.style.display = "flex";
+}
+
+accordionContentButtons.forEach((button)=>{	
+	button.addEventListener("click", ()=> {
+		closeAndDisplayThanks();
+	})
+});
+
+accordionButton.addEventListener("click", toggleAccordionContent);
+
+
 
 
 
